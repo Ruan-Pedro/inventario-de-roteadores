f5163af5c8bc93d8499e6d7a175db417601bf3ba
Ruan Pedro Mendes <72527894+Ruan-Pedro@users.noreply.github.com>
2021-08-30 20:52:21 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+//  const fs = require('fs')
+//  const bodyParser = require('body-parser')
+const apiRoutes = require('./routes/api')
+
+const app = express()
+const PORT = 5050
+
+app.use('/',express.static(path.join(__dirname, "client")))
+app.use('/api',apiRoutes)
+ app.use('/database', express.static(path.join(__dirname, "public/database.html")))
+//  app.use('/database/style.css', express.static(path.join(__dirname, "public/styles/style.css")))
+
+ app.use('/dashboard', express.static(path.join(__dirname, "public/dashboard.html")))
+app.use('/management',express.static(path.join(__dirname, "public/management.html")))
+
+// app.get('/', (req,res)=>{
+// res.send(express.static(path.join(__dirname + "client")))
+// })
+
+
+// app.get('/', (req,res)=>{
+//     res.send('funcionou amigão')
+// })
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
